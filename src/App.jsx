dc3623051f4f94ebae0f3e1b593890a9e1b02f5c
owlsspell <arhipowael@gmail.com>
2024-03-21 17:33:33 +0200

    create home page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MilkyWay from './components/MilkyWay'
+
+function App() {
+
+  return (
+    <>
+      <MilkyWay />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MilkyWay.jsx
@@ -0,0 +1,40 @@
+import milky_way from '../assets/images/milky_way.jpg'
+import mountain_1 from '../assets/images/mountain_1.png'
+import scroll from '../assets/images/scroll.svg'
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+
+export default function MilkyWay() {
+    gsap.registerPlugin(useGSAP);
+
+    const container = useRef();
+    gsap.defaults({ transition: 1 })
+
+    useGSAP(() => {
+        gsap.from(".mountain", { scale: 1.5, yPercent: -25, duration: 2 });
+        gsap.from(".way", { scale: 2, xPercent: 30, yPercent: -60, duration: 2 });
+        gsap.from(".mainTitle", { opacity: 0, translateY: -150, duration: 0.8 });
+
+        gsap.timeline()
+            .from(".subTitle", { opacity: 0, translateY: -150, duration: 1.2 })
+            .to(".text", { opacity: 1, duration: 1 })
+            .to(".scrollBtn", { opacity: 1, duration: 0.3 })
+            .to(".scrollBtn", { y: -50, repeat: -1, yoyo: true, duration: 0.5, ease: "bounce.inOut" });
+        // .to(".scrollBtn", { bottom: 200, repeat: -1, duration: 1, });
+    }, { scope: container });
+
+    return (
+        <div className='relative min-h-screen w-auto overflow-hidden' ref={container}>
+            <img className='absolute way object-cover h-screen w-full' src={milky_way} alt="" />
+            <img className="absolute top-0 mountain object-cover h-screen w-full" src={mountain_1} alt='' />
+            <div className='flex items-center flex-col mt-20 mx-32 gap-4 z-10 relative h-screen text-white'>
+                <h3 className="text-xl subTitle">ACROSS THE MILKY WAY</h3>
+                <h1 className="text-5xl mb-2 font-semibold mainTitle">STARRY NIGHT</h1>
+                <p className='font-thin text opacity-0'>Lorem ipsum dolor sit amet consectetur. Integer tempor amet diam laoreet. Amet volutpat ac lacus platea fermentum. Velit mauris orci ut sed ornare. Platea lectus fringilla elementum blandit lacinia dignissim etiam tellus morbi.Lorem ipsum dolor sit amet consectetur. Integer tempor amet diam laoreet. Amet volutpat ac lacus platea fermentum. Velit mauris orci ut sed ornare. Platea lectus fringilla elementum blandit lacinia dignissim etiam tellus morbi.</p>
+            </div>
+            <img className='absolute left-1/2 bottom-24 h-24 scrollBtn opacity-0' src={scroll} alt="" />
+        </div>
+    )
+}
